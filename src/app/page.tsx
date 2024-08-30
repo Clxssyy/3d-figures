53ac845bac6e57836d36bf47ee631e3a3956ac59
Michael Connolly <mpc63@uakron.edu>
2024-08-30 12:24:05 -0400

    Add starting navbar structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
@@ -5,9 +6,28 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex h-full w-full place-items-center justify-center font-bold text-black">
-        Test
+      <nav className="grid h-10 w-full grid-cols-3">
+        <section className="flex place-items-center bg-red-500 px-2">
+          <Link href="">Logo</Link>
+        </section>
+        <section className="flex place-items-center justify-center gap-2 bg-sky-500">
+          <Link href="">Home</Link>
+          <Link href="">Shop</Link>
+          <Link href="" className="animate-pulse font-bold text-red-500">
+            Claim!
+          </Link>
+          <Link href="">Collection</Link>
+        </section>
+        <section className="flex place-items-center justify-end bg-yellow-500 px-2">
+          <Link href="">1</Link>
+          <Link href="">2</Link>
+          <Link href="">3</Link>
+        </section>
+      </nav>
+      <main className="flex h-full w-full flex-col">
+        <h1>3D Figures</h1>
       </main>
+      <footer></footer>
     </HydrateClient>
   );
 }
